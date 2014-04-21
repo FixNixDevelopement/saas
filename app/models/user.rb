f29c8fce2358927d730fedcbca69e47ab93aeac9
santu47 <vudugulasantu@gmail.com>
2014-04-21 13:36:35 +0530

    second commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,16 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable , request_keys: [:subdomain]
+         :recoverable, :rememberable, :trackable, :validatable 
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
 
 
-  def self.find_for_authentication(warden_conditions)
-    where(:email => warden_conditions[:email], :subdomain => warden_conditions[:subdomain]).first
-  end
+  # def self.find_for_authentication(warden_conditions)
+  #   where(:email => warden_conditions[:email], :subdomain => warden_conditions[:subdomain]).first
+  # end
 
-  
+
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Subdevise::Application.config.session_store :cookie_store, key: '_subdevise_session' , domain: '.example.com'
+Subdevise::Application.config.session_store :cookie_store, key: '_subdevise_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
